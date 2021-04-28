8029c77e94dadec6699cf923394917998a4fb38a
Tiffany Zhu <zhz90@pitt.edu>
2021-04-28 18:52:19 -0400

    Update SelfJoin.sql

--- SelfJoin.sql
+++ SelfJoin.sql
@@ -5,15 +5,13 @@ Show the bus no. and company for the first bus, the name of the stop for the tra
 
 SELECT c.num, c.company, c.endstop AS transferstop, l.num, l.company
 FROM (SELECT a.num, a.company, stopa.name AS startstop, stopb.name AS endstop
-FROM route a JOIN route b ON
-  (a.company=b.company AND a.num=b.num)
-  JOIN stops stopa ON (a.stop=stopa.id)
-  JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name='Craiglockhart') c JOIN 
-(SELECT a.num, a.company, stopa.name AS startstop, stopb.name AS endstop
-FROM route a JOIN route b ON
-  (a.company=b.company AND a.num=b.num)
-  JOIN stops stopa ON (a.stop=stopa.id)
-  JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopb.name='Lochend') l 
-ON (c.endstop=l.startstop)
+      FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+                   JOIN stops stopa ON (a.stop=stopa.id)
+                   JOIN stops stopb ON (b.stop=stopb.id)
+      WHERE stopa.name='Craiglockhart') c JOIN 
+     (SELECT a.num, a.company, stopa.name AS startstop, stopb.name AS endstop
+      FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+                   JOIN stops stopa ON (a.stop=stopa.id)
+                   JOIN stops stopb ON (b.stop=stopb.id)
+      WHERE stopb.name='Lochend') l 
+      ON (c.endstop=l.startstop)
