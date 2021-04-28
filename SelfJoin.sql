0092843fbc55faaeb0fd6ed0175ec54d8bf70590
Tiffany Zhu <zhz90@pitt.edu>
2021-04-28 18:57:50 -0400

    Update SelfJoin.sql

--- SelfJoin.sql
+++ SelfJoin.sql
@@ -1,4 +1,11 @@
 /*
+
+Edinburgh Buses
+- stops(id, name)
+- route(num, company, pos, stop)
+Details of the database: https://sqlzoo.net/wiki/Edinburgh_Buses.
+
+Question:
 Find the routes involving two buses that can go from Craiglockhart to Lochend.
 Show the bus no. and company for the first bus, the name of the stop for the transfer, and the bus no. and company for the second bus.
 */
