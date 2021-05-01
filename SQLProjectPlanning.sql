47e3865cc2b2ca892b079db791cae5a65437bea1
Tiffany Zhu <zhz90@pitt.edu>
2021-05-01 19:02:13 -0400

    Update SQLProjectPlanning.sql

--- SQLProjectPlanning.sql
+++ SQLProjectPlanning.sql
@@ -13,4 +13,26 @@ If there is more than one project that have the same number of completion days, 
 
 */
 
+SELECT Project_Start_Date, Project_End_Date
+FROM (SELECT DISTINCT IF(P.S_Date IS NULL, LAG(P.S_Date) OVER(), P.S_Date) AS Project_Start_Date,
+                IF(P.E_Date IS NULL, LEAD(P.E_Date) OVER(), P.E_Date) AS Project_End_Date
+    FROM(SELECT IF(P2.Task_ID IS NULL, P1.Start_Date, NULL) AS S_Date,
+                IF(P3.Task_ID IS NULL, P1.End_Date, NULL) AS E_Date 
+         FROM Projects P1 LEFT JOIN Projects P2 ON P1.Start_Date = P2.End_Date
+                          LEFT JOIN Projects P3 ON P1.End_Date = P3.Start_Date
+        WHERE P2.Task_ID IS NULL OR P3.Task_ID IS NULL) AS P
+    ) AS Project
+ORDER BY Project_End_Date - Project_Start_Date, Project_Start_Date
 
+
+/* To get the start date of each project */                          
+SELECT P1.Start_Date
+FROM Projects P1 LEFT JOIN Projects P2 ON P1.Start_Date = P2.End_Date
+WHERE P2.Task_ID IS NULL
+
+/* To get the end date of each project */
+SELECT P1.End_Date 
+FROM Projects P1 LEFT JOIN Projects P3 ON P1.End_Date = P3.Start_Date
+WHERE P3.Task_ID IS NULL
+                          
+                          
