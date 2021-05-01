5cce513542f1994c903364673c3c7eb43356ee89
Tiffany Zhu <zhz90@pitt.edu>
2021-05-01 03:59:59 -0400

    Update Inteviews.sql

--- Inteviews.sql
+++ Inteviews.sql
@@ -1,7 +1,7 @@
 /*
-https://www.hackerrank.com/challenges/sql-projects/problem
+https://www.hackerrank.com/challenges/interviews/problem
 
-SQL Project Planning - Inteviews 
+Inteviews 
 
 Samantha interviews many candidates from different colleges using coding challenges and contests. 
 Write a query to print the contest_id, hacker_id, name, and the sums of total_submissions, total_accepted_submissions, total_views, and total_unique_views for each contest sorted by contest_id. Exclude the contest from the result if all four sums are .
