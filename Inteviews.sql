9850e07a87cefb46f36061aedeb1224cdc8fd8a6
Tiffany Zhu <zhz90@pitt.edu>
2021-05-01 03:55:47 -0400

    Update and rename SQLProjectPlanning.sql to Inteviews.sql

--- Inteviews.sql
+++ Inteviews.sql
@@ -1,7 +1,7 @@
 /*
 https://www.hackerrank.com/challenges/sql-projects/problem
 
-SQL Project Planning
+SQL Project Planning - Inteviews 
 
 Samantha interviews many candidates from different colleges using coding challenges and contests. 
 Write a query to print the contest_id, hacker_id, name, and the sums of total_submissions, total_accepted_submissions, total_views, and total_unique_views for each contest sorted by contest_id. Exclude the contest from the result if all four sums are .
